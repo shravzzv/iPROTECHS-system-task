a418e409b97dc14bc40981aa8f9eb94e8558eb46
sai shravan <saishravan384@gmail.com>
2024-12-30 21:34:28 +0530

    feat: Implement authentication routes and controller for signup and login

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 
 require('./config/db.config')
 const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
+const authRouter = require('./routes/auth')
 
 const app = express()
 
@@ -22,7 +22,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/auth', authRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ controllers/authController.js
@@ -0,0 +1,128 @@
+const asyncHandler = require('express-async-handler')
+const { body, validationResult, matchedData } = require('express-validator')
+const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+exports.signupGet = (req, res) => {
+  res.send('signup not implemented')
+}
+
+exports.signupPost = [
+  body('email')
+    .trim()
+    .notEmpty()
+    .withMessage('Email must not be empty.')
+    .bail()
+    .isEmail()
+    .withMessage('Email is not a valid email address.')
+    .escape()
+    .custom(async (email) => {
+      const existingUser = await User.findOne({ email }, '_id')
+      if (existingUser) throw new Error(`E-mail already in use.`)
+    }),
+
+  body('password')
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage('Password must be atleast 8 characters long.'),
+
+  body('passwordConfirm')
+    .trim()
+    .notEmpty()
+    .withMessage('Password confirm must not be empty.')
+    .bail()
+    .custom((value, { req }) => {
+      return value === req.body.password
+    })
+    .withMessage(`Doesn't match the password.`),
+
+  body('role').trim().notEmpty().withMessage('Role must not be empty.'),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req)
+    const { email, password, passwordConfirm, role } = matchedData(req, {
+      onlyValidData: false,
+    })
+
+    const newUser = new User({
+      email,
+      password,
+      role,
+    })
+
+    if (errors.isEmpty()) {
+      const hashedPassword = await bcrypt.hash(password, 10)
+      newUser.password = hashedPassword
+      await newUser.save()
+
+      const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
+        expiresIn: '7d',
+      })
+
+      res.json({ token, userId: newUser.id })
+    } else {
+      res.render('landing/signupForm', {
+        title: 'Sign Up Error',
+        errors: errors.array(),
+        email,
+        password,
+        passwordConfirm,
+        role,
+      })
+    }
+  }),
+]
+
+exports.loginGet = (req, res) => {
+  res.send('login not implemented')
+}
+
+exports.loginPost = [
+  body('email')
+    .trim()
+    .notEmpty()
+    .withMessage('Email must not be empty.')
+    .bail()
+    .isEmail()
+    .withMessage('Email is not a valid email address.')
+    .bail()
+    .escape()
+    .custom(async (email, { req }) => {
+      const user = await User.findOne({ email }, '_id password')
+      if (user) req.user = user
+      if (!user) throw new Error(`Email ${email} doesn't exist.`)
+    }),
+
+  body('password')
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage('Password must be atleast 8 characters long.')
+    .bail()
+    .custom(async (password, { req }) => {
+      if (req.user && !(await bcrypt.compare(password, req.user.password))) {
+        throw new Error('Incorrect password.')
+      }
+    }),
+
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req)
+
+    if (errors.isEmpty()) {
+      const token = jwt.sign({ id: req.user.id }, process.env.JWT_SECRET, {
+        expiresIn: '7d',
+      })
+
+      res.json({ token, userId: req.user.id })
+    } else {
+      const { email, password } = matchedData(req, { onlyValidData: false })
+
+      res.render('landing/signinForm', {
+        title: 'Sign In Error',
+        errors: errors.array(),
+        email,
+        password,
+      })
+    }
+  }),
+]
