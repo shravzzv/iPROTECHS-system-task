a890c6623e9001a64c8d88e13baef3bd8b543f69
sai shravan <saishravan384@gmail.com>
2024-12-31 12:46:37 +0530

    feat: Simplify MongoDB connection options by removing deprecated parameters

--- config/db.config.js
+++ config/db.config.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   mongoose.set('strictQuery', false)
   await mongoose
-    .connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(process.env.MONGO_URI)
     .catch((err) => console.error(err))
 }
 
