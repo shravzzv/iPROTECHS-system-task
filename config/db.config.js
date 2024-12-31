c0dfeefb3adf02ba1bd82561cda4dce9c7bb41f0
sai shravan <saishravan384@gmail.com>
2024-12-31 11:39:09 +0530

    feat: Update MongoDB connection options for improved compatibility

--- config/db.config.js
+++ config/db.config.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   mongoose.set('strictQuery', false)
   await mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .catch((err) => console.error(err))
 }
 
